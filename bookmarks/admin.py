c6f84fd19f2a6afdf2d03af4911fae09d769cc4f
gayamsahithi <39076467+gayamsahithi@users.noreply.github.com>
2018-05-29 08:33:32 +0530

    added bookmark model

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Link
+from .models import Bookmark, Link
 # Register your models here.
 admin.site.register(Link)
+admin.site.register(Bookmark)
